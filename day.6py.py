c64907256269edbeeb551d20f3beffe4d9112bd2
aaashishguptaa4792-lab <aaashishguptaa4792@gmail.com>
2026-03-10 12:44:41 +0530

    Add files via upload

---
+++ day.6py.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  9 13:29:51 2026
+
+@author: hirwa
+"""
+print ("hello world")
+
+#variables
+number =2
+print ("number")
+
+Name = "akshara"
+print ("name")
+print("name[4:8]")
+
+number=22
+print(number)
+
+integer_var=9
+string_var="this is python class" 
+string_var[3:7]
+float_var=3.14
+
+types of operators
+arithmetic operators
+
+print(9*4)
+print(9+4)
+print(9-4)
+print(9/4)
+print(9%4)
+print(9//4)
+
+#calculate it the candidate is eligible to vote
+
+Age_of_candidate=int(input("enter your age:"))
+
+if Age_of_candidate>18:
+    print("congrats you are eligible to vote!!")
+    
+else:
+    print("sorry! your are not eligible to vote")
+    
+
+Data types
+Tuple()
+var_tuple=(1,2,3,4,5)
+print(var_tuple)
+print(type(var_tuple))
+
+List[]
+var_list=[1,2,3,4,5,6]
+print(var_list)
