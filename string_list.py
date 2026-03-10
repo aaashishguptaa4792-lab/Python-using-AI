f5f65b892f02be6b5ff1aaeca7feedeaca7c029a
aaashishguptaa4792-lab <aaashishguptaa4792@gmail.com>
2026-03-10 12:45:28 +0530

    Add files via upload

---
+++ string_list.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 16 11:21:01 2026
+
+@author: hirwa
+"""
+    
+
+#convert string into a list of characters
+s = "This is a python program"
+lst=list(s)
+print(s)
+print(lst)
+list_variables =s.split()
+print(list_variables)  
+s1= "Apple,banana,grape"
+s1.split(",")                                                                                                                                               
+         
+
+
+
